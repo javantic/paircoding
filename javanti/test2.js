898cbb6e3d57b51567025c1781d15d14195a7ae7
Javanti C <javanti-chauhan@live.nmit.ac.nz>
2022-04-06 09:32:50 +1200

    initial

--- javanti/test2.js
+++ javanti/test2.js
@@ -1,2 +1,9 @@
-Random comment test
-wfdas
+//create a function that takes an array containing only numbers and return the first element.
+//Example: getArrElement([1,2,3,4]) -> 1
+
+// function getArrElement(arr) {
+//     return arr(o)
+// }
+// console.log(getArrElement(1,2,3,4))
+
+const getArrElement = arr => arr[0] 
