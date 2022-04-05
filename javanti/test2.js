615500fd8dd3c6c5d56b5517ed4aa58bdfc28b83
Javanti C <javanti-chauhan@live.nmit.ac.nz>
2022-04-06 11:31:09 +1200

    update2

--- javanti/test2.js
+++ javanti/test2.js
@@ -10,9 +10,37 @@
 
 //Give two arguments, return an array which contains these two arguments 
 
-function createArray(a,b) {
-    var arr = [a,b]
+// function createArray(a,b) {
+//     var arr = [a,b]
 
-    return arr
-}
-console.log(createArray(1,2))
+//     return arr
+// }
+// console.log(createArray(1,2))
+
+//Write a function to reverse an array
+//Example: reverseArray([1,2,3,4]) -> [4,3,2,1]
+
+// const array1 = [1,2,3,4];
+// console.log(array1);
+// const reversed = array1.reverse();
+// console.log(reversed);
+
+// function reverseArray(array1) {
+//     return array1.reverse()
+
+// }
+// console.log(reverseArray([1,2,3,4,5,6,7,8]))
+
+//Create a function that accepts an array and return the last element
+//Example: lastElement(["Ali, 12, Blue, Green"]) -> green
+
+// function lastElement(arr) {
+//     return arr.pop()
+// }
+
+
+//create a function that takes an array and returns it as a string
+
+var arr = (1,2,3,4,5,6,7)
+
+ArrayToString(["Javanti, 12, Green"])
